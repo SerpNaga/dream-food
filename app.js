eeb50999bb1e8843d261f1efc7a1930f93e354b7
SerpNaga <TyurinaAA20@st.ithub.ru>
2021-11-19 13:12:19 +0300

    2

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const port = process.env.PORT || 3002
+const serverRouter = require("./server/routes/mainRouter")
+const stylus = require("stylus")
 
 const app = express()
 
-app.get("/", (req, res)=>{
-    res.send("<h1>Dream Food</h1>")
-})
+app.use(stylus.middleware({
+    src: "./public",
+    dest: "./public"
+}))
+
+
+app.set("views", "./server/views")
+app.set("view engine", "pug")
+
+app.use(express.static("./public"))
+
+app.use("/", serverRouter)
 
 app.listen(port)
