6b1e045af95b8c884993d11ad5744769f4cc289d
SerpNaga <TyurinaAA20@st.ithub.ru>
2021-12-11 20:08:57 +0300

    h

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,8 +1,30 @@
 const router = require("express").Router()
+const fs = require("fs")
+
+
+
+let data = ""
+const readFile = (path)=>{
+    return fs.readFileSync(path, "utf-8")
+}
+data = readFile("./data/fruits.csv")
+data = data.split("\r\n")
+
+const Product = function(prArr){
+    names = data[0].split(";")
+    names.forEach((name, i)=>{this[name] = prArr[i]})
+}
+const products = []
+for (let i =1; i<data.length;i++){
+    products.push(new Product(data[i].split(";")))
+}
+console.log(products)
+
 
 router.get("/", (req, res)=>{
     res.render("index", {
-        title: "Здоровый образ жизни"
+        title: "Здоровый образ жизни",
+        products: products
     })
 })
 
